4aef5619c0cec5fd7bbe5683a4e522b9d9e6d686
CrazzyGaming <ionut_pustiul.thau@yahoo.com>
2018-05-18 14:16:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!ip') {
-    	message.reply('Serverul este in lucru.');
+    	message.reply('Serverul este in lucru, deschiderea va avea loc pe data de 16.06.2018.');
   	}
 });
 
